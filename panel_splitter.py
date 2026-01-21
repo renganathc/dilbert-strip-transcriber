e0479bca0842842c098a9152dbfc939a5bf1474e
Renganath Chokkalingam <rengnath1234@gmail.com>
2026-01-21 08:31:41 +0530

    fix issues with returning horizontal white regions in strip

--- panel_splitter.py
+++ panel_splitter.py
@@ -2,34 +2,40 @@ import cv2
 import numpy as np
 from loader import load_strips
 
+def line_to_region(white_lines):
+    regions = []
+    if not white_lines:
+        return []
+    
+    region = [white_lines[0], white_lines[0]]
+    for i in range(1,len(white_lines)):
+        if white_lines[i] == white_lines[i-1] + 1:
+            region[1] = white_lines[i]
+        else:
+            regions.append(region)
+            region = [white_lines[i], white_lines[i]]
+
+    regions.append(region)
+    return regions
+
 def split(img):
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    #print(img_gray.shape)
     h, w = img_gray.shape
 
-    empty_lines = []
-    consecutive = 0
+    white_lines = []
 
     for i in range(h):
-        whites = 0
+        white_pixels = 0
         for j in range(w):
             if img_gray[i][j] > 220:
-                whites += 1
-        if whites/w > 0.998:
-            if len(empty_lines) > 0 and empty_lines[len(empty_lines) - 1] == i - consecutive - 1 and i != h - 1:
-                consecutive += 1
-                continue
-            
-            empty_lines.append(i)
+                white_pixels += 1
+        if white_pixels/w > 0.998:
+            white_lines.append(i)
         
-        if consecutive != 0:
-            empty_lines.append(i)
-            consecutive = 0
-        
-    print(empty_lines)
+    print("blank regions: ", line_to_region(white_lines))
 
-    #cv2.imshow("comic strip", img_gray)
-    #cv2.waitKey(0)
+    cv2.imshow("comic strip", img_gray)
+    cv2.waitKey(0)
 
 
 strips = load_strips("dilbert_1989_to_2023")
@@ -37,3 +43,4 @@ strips = load_strips("dilbert_1989_to_2023")
 for strip in strips:
     image = strip["image"]
     split(image)
+    # press any key to iterate through the imaegs and see blan k regions
