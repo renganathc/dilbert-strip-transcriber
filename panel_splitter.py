d639d7cbf2086d35a5a31381285281b5b2ad57c8
Renganath Chokkalingam <rengnath1234@gmail.com>
2026-01-21 17:44:06 +0530

    Finalize panel splitter that segments the comic strips into panels

--- panel_splitter.py
+++ panel_splitter.py
@@ -2,23 +2,37 @@ import cv2
 import numpy as np
 from loader import load_strips
 
-def line_to_region(white_lines):
-    regions = []
+def lines_to_spaces(white_lines):
+    spaces = []
     if not white_lines:
         return []
     
-    region = [white_lines[0], white_lines[0]]
+    space = [white_lines[0], white_lines[0]]
     for i in range(1,len(white_lines)):
         if white_lines[i] == white_lines[i-1] + 1:
-            region[1] = white_lines[i]
+            space[1] = white_lines[i]
         else:
-            regions.append(region)
-            region = [white_lines[i], white_lines[i]]
+            spaces.append(space)
+            space = [white_lines[i], white_lines[i]]
 
-    regions.append(region)
-    return regions
+    spaces.append(space)
+    return spaces
 
-def horizontal_split(img):
+def strip_cropper(spaces, dim_length):
+    splits = [0]
+    for space in spaces:
+        splits.append((space[0] + space[1])//2)
+    splits.append(dim_length - 1)
+    
+    sub_strips = []
+    max_dim = max([splits[i] - splits[i-1] for i in range(1, len(splits))])
+    for i in range(1, len(splits)):
+        if splits[i] - splits[i-1] > 0.3*max_dim:
+            sub_strips.append([splits[i-1], splits[i]])
+
+    return sub_strips
+
+def horizontal_splitter(img):
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     h, w = img_gray.shape
     white_lines = []
@@ -31,32 +45,48 @@ def horizontal_split(img):
         if white_pixels/w > 0.997:
             white_lines.append(i)
         
-    print("blank regions (horizontal): ", line_to_region(white_lines))
-    return line_to_region(white_lines)
+    blank_regions = lines_to_spaces(white_lines)
+    sub_strips = strip_cropper(blank_regions, h)
+
+    return [img[x[0]:x[1],:] for x in sub_strips]
+
     
-def vertical_split(img):
+def vertical_splitter(img):
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     h, w = img_gray.shape
     white_lines = []
 
     for j in range(w):
         white_pixels = 0
-        for i in list(range(3*h//20)) + list(range(19*h//20, h)):
+        for i in list(range(3*h//20+1)) + list(range(19*h//20, h)):
             if img_gray[i][j] > 230:
                 white_pixels += 1
-        if white_pixels/(0.20*h) > 0.99:
-            white_lines.append(j)
+        if white_pixels/(0.20*h) > 0.98:
+            white_lines.append(j)     
+    blank_regions = lines_to_spaces(white_lines)
+    panels = strip_cropper(blank_regions, w)
+
+    return [img[:,x[0]:x[1]] for x in panels]
+
+
+def panelizer(strip):
+    panels = []
+    image = strip["image"]
+    sub_strips = horizontal_splitter(image)
+
+    for sub_strip in sub_strips:
+        panels = panels + vertical_splitter(sub_strip)
         
-    print("blank regions (vertical): ", line_to_region(white_lines))
-    return line_to_region(white_lines)
+    return panels
+
 
 
 strips = load_strips("dilbert_1989_to_2023")
 
 for strip in strips:
-    image = strip["image"]
-    horizontal_split(image)
-    vertical_split(image)
-    cv2.imshow("comic strip", image)
+    print(strip["date"])
+    panels = panelizer(strip)
+    for i in range(len(panels)):
+        cv2.imshow(str(i), panels[i])
+        cv2.waitKey(300)
     cv2.waitKey(0)
-    # press any key to iterate through the imaegs and see blan k regions
