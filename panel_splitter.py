3a7592367d178ca94df7a2dab707528665c6cac0
Renganath Chokkalingam <rengnath1234@gmail.com>
2026-01-21 07:53:49 +0530

    Improve splitter output by returning white gutters in strip

--- panel_splitter.py
+++ panel_splitter.py
@@ -8,33 +8,32 @@ def split(img):
     h, w = img_gray.shape
 
     empty_lines = []
+    consecutive = 0
 
     for i in range(h):
         whites = 0
-        consecutive = 0
         for j in range(w):
-            if img_gray[i][j] > 230:
+            if img_gray[i][j] > 220:
                 whites += 1
         if whites/w > 0.998:
+            if len(empty_lines) > 0 and empty_lines[len(empty_lines) - 1] == i - consecutive - 1 and i != h - 1:
+                consecutive += 1
+                continue
+            
             empty_lines.append(i)
-
+        
+        if consecutive != 0:
+            empty_lines.append(i)
+            consecutive = 0
+        
     print(empty_lines)
 
-    cv2.imshow("comic strip", img_gray)
-    cv2.waitKey(0)
-
-    if 0 in empty_lines:
-        print(empty_lines)
-        cv2.imshow("efh", img_gray)
-        cv2.waitKey(0)
-        return True
-
-    return 0
+    #cv2.imshow("comic strip", img_gray)
+    #cv2.waitKey(0)
 
 
 strips = load_strips("dilbert_1989_to_2023")
 
 for strip in strips:
     image = strip["image"]
-    if split(image) is True:
-        break
+    split(image)
